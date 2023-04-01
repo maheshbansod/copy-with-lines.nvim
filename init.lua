ab072ef14c994ed6ab523a4ddb42e8142c6f657c
Light <coolkevin011@gmail.com>
2023-04-01 22:55:28 +0530

    avoid loading if already loaded

--- init.lua
+++ init.lua
@@ -1,4 +1,9 @@
 
+if vim.g.loaded_nvim_copy_with_lines then
+	return
+end
+vim.g.loaded_nvim_copy_with_lines = true
+
 local c = require('copy-with-lines');
 
 vim.api.nvim_create_user_command("CopyWithLineNumbers", c.CopyWithLineNumbers, {
